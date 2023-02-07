dc15ec2dd19173197c07bc1b7978e15539cc842f
AppVeyr <ci@appveyor.com>
2023-02-07 17:34:01 +0300

    f

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Widget from './Widget';
 
 const widget = new Widget(
   document.getElementById('root'),
-  'https://ahj-11-1-rxjs-poling-sergius.herokuapp.com/messages/unread',
+  'https://github.com/maria-namira/ahj-rxjs_poling.git',
 );
 
 widget.subscribeToUpdate();
